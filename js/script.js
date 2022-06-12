d76ab0a33f393d6f412f323b5f22f17df0d0e697
John Yuill <john@fig4.com>
2022-06-11 17:46:21 -0700

    slick demo home page

--- js/script.js
+++ js/script.js
@@ -1,5 +1,13 @@
 
+function btnClick(){
+    alert("You clicked the button - you're a champ.");
+}
+function btnChgClick(){
+    document.getElementById('tgtTag').innerHTML="Hello, my name is John! I have a bicycle. I want to pay the check, please.";
+}   
+
 function btnClick2(){
+
     alert("Clicked (message from js file).");
 }
             
