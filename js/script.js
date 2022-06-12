c312db34ea34740d45ec31017e7a3c97ec219d7c
John Yuill <john@fig4.com>
2022-06-12 14:42:14 -0700

    using js to tweak navbar

--- js/script.js
+++ js/script.js
@@ -10,4 +10,23 @@ function btnClick2(){
 
     alert("Clicked (message from js file).");
 }
+
+// function for highlighting active page in nav
+// using jQuery
+$(function(){
+    // list pages - preferably in order of nav presentation
+    var pages = ['index.html','about','contact','|','list-tbl','index-v3'];
+    var pathname = window.location.pathname;
+
+    $('.nav-link').each(function(i){
+        // if path matches item in page array, set class and attributes to active
+        if(pathname.includes(pages[i])){
+            $(this).addClass('active');
+            $(this).attr('aria-current','page');
+        // if class already set to active, remove new setting
+        } else if(this.className.includes('active')){
+            $(this).removeClass('active');
+        }
+    });
+});
             
